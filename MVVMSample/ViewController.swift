7016785b225a01d668abaa80c4365f963f67f318
osada <osadab6mb@gmail.com>
2024-03-25 19:31:00 +0900

    Initial Commit

---
+++ MVVMSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by 長田公喜 on 2024/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
